3b231d4886c770ba7d715b0cca4138a4e6976d5c
faradisyulianto20 <faradisy20@gmail.com>
2025-06-27 22:40:11 +0700

    add package.json

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const names = require('./utils')
-const sayHi = require('./function')
-require('./sum')
-const sum = require('./sum')
+// const names = require('./utils')
+// const sayHi = require('./function')
+// require('./sum')
+// const sum = require('./sum')
 
-console.log(sum);
+// console.log(sum);
 
 
-console.log(names);
+// console.log(names);
 
-sayHi(names.allnames.yulianto)
+// sayHi(names.allnames.yulianto)
 
-console.log(sayHi)
+// console.log(sayHi)
+
+const _ = require('lodash')
+
+const items = [1, [2, 3, [4]]]
+
+const newItems = _.flattenDeep(items);
+
+console.log(newItems)
+
 
