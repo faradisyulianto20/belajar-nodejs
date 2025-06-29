39918f11e68a7af10c6ed2effd01435e89057ab7
faradisyulianto20 <faradisy20@gmail.com>
2025-06-29 14:30:38 +0700

    end node session

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,1 +1,35 @@
-console.log('Express Tutorial')
+// console.log('Express Tutorial')
+// console.log('Tes nodemon');
+
+
+const http = require('http')
+
+const {readFileSync} = require('fs')
+
+const homePage = readFileSync('./index.html')
+const homeStyle = readFileSync('./style.css')
+
+const server = http.createServer((req, res)=> {
+    // console.log(req.method);
+    // console.log(req.url);
+    const url = req.url
+    if(url === '/'){
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+    else if (url === '/style.css'){
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(homeStyle)
+        res.end()
+    }
+    else {
+        res.writeHead(404, {'content-type':'text/html'})
+        res.write('<h1>Page is not Found</h1>')
+        res.end()
+    }
+    // console.log('user hit the server');
+})
+
+server.listen(3000)
+
